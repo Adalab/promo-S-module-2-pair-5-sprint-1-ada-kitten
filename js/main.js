48509b20dc0c2fb5a53b58747506a3ff0a2899bf
Patuco <patricia.beco.dg@gmail.com>
2023-02-03 10:42:57 +0100

    Add description variables

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@
 
 const showForm = document.querySelector('.js-new-form');
 showForm.classList.remove('collapsed');
+
+const input_search_desc = document.querySelector(".js_in_search_desc");
+const descrSearchText = input_search_desc.value;
